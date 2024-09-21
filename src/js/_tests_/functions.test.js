4f4ff01a9322ded0999c0880d2d83c32385ae752
Udav-94 <ups-fti@yandex.ru>
2024-09-21 15:50:39 +0300

    Переделан тест

--- src/js/_tests_/functions.test.js
+++ src/js/_tests_/functions.test.js
@@ -1,20 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+
 /* import { validater, inputLimitation, addCurtain,  inputNumLimitation} from '../functions'; */
-import  inputNumLimitation from '../functions';
-/* const { inputNumLimitation } = require('../functions'); */
+import  {inputNumLimitation} from '../functions';
+
 
 test ('Test of function of number length', () => {
     let res1 = inputNumLimitation('4916283256211951');
-/*     let res2 = inputNumLimitation('491628325621195123');
-    let res3 = inputNumLimitation('49162832562'); */
+    let res2 = inputNumLimitation('491628325621195123');
+    let res3 = inputNumLimitation('49162832562');
 
-    /* let res1 = true; */
+    /*window.alert = jest.fn(res2);
+    /* console.log(res2); */
+    /*inputNumLimitation('491628325621195123');
+    jest.spyOn(window, 'alert').mockImplementation((e) => {console.log(e)});
+    /* expect(qwe).toBe('Вы ввели лишние цифры'); */
+
 
     expect(res1).toEqual(true);
-/*     expect(res2).toBe(false);
-    expect(res3).toBe(false); */
-
-/*     let res1 = 'qw';
-
-    expect(res1).toBe('qw'); */
+    expect(res2).toBe(false);
+    expect(res3).toBe(false);
 
 })
